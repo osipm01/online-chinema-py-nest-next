d0ca2baa34309bc4073a835ce5599047146663d5
osipm01 <osip2004@vk.com>
2026-09-06 22:02:05 +0300

    init

---
+++ mainservice/prisma/migrations/20260903184141_init/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - Added the required column `accessToken` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `refreshToken` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "accessToken" TEXT NOT NULL,
+ADD COLUMN     "avatar" TEXT,
+ADD COLUMN     "bio" TEXT,
+ADD COLUMN     "phone" TEXT,
+ADD COLUMN     "refreshToken" TEXT NOT NULL;
---
+++ mainservice/src/users/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+
+  constructor(private prisma: PrismaService)
+
+  create(createUserDto: CreateUserDto) {
+
+
+
+  }
+}
